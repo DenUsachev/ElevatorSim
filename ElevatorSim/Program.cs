1fa9422b3b5264e5c9092a0eec5f8877fa0919c7
Denis Usachev <dusachev@gofura.com>
2017-12-22 09:00:08 +0300

    Project Init

---
+++ ElevatorSim/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ElevatorSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
